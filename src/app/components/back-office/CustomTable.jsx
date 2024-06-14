4185ac98bad6b5be6e65b393d62ac06732cef7c5
Philip Harteveld <philipharteveld3110@gmail.com>
2024-06-14 21:01:44 +0200

    charts.js drop down sidebar

--- src/app/components/back-office/CustomTable.jsx
+++ src/app/components/back-office/CustomTable.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import data from "../../data.json";
 
 export default function CustomTable() {
+  const PAGE_SIZE = 10;
+  const tabledata = data;
+  const [currentPage, setCurrentPage] = useState(1);
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+  const currentlyDisplayedData = tabledata.slice(startIndex, endIndex);
+  const numberOfPages = tabledata.lenght / 10;
+
+  function handlePageChange(page) {
+    console.log(page);
+  }
+
   return (
     <div>
       Recent Orders
@@ -22,16 +36,16 @@ export default function CustomTable() {
                   </div>
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Product name
+                  ID
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Color
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Category
+                  Ordered Date
                 </th>
                 <th scope="col" className="px-6 py-3">
                   Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Customer Name
                 </th>
                 <th scope="col" className="px-6 py-3">
                   Action
@@ -39,37 +53,47 @@ export default function CustomTable() {
               </tr>
             </thead>
             <tbody>
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td className="w-4 p-4">
-                  <div className="flex items-center">
-                    <input
-                      id="checkbox-table-search-1"
-                      type="checkbox"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label for="checkbox-table-search-1" className="sr-only">
-                      checkbox
-                    </label>
-                  </div>
-                </td>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Apple MacBook Pro 17
-                </th>
-                <td className="px-6 py-4">Silver</td>
-                <td className="px-6 py-4">Laptop</td>
-                <td className="px-6 py-4">$2999</td>
-                <td className="px-6 py-4">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              {currentlyDisplayedData.map((item, i) => {
+                return (
+                  <tr
+                    key={item}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
                   >
-                    Edit
-                  </a>
-                </td>
-              </tr>
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-1"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                        <label
+                          for="checkbox-table-search-1"
+                          className="sr-only"
+                        >
+                          checkbox
+                        </label>
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      {item.order_id}
+                    </th>
+                    <td className="px-6 py-4">{item.order_date}</td>
+                    <td className="px-6 py-4">{item.total_amount}</td>
+                    <td className="px-6 py-4">{item.customer_name}</td>
+                    <td className="px-6 py-4">
+                      <a
+                        href="#"
+                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
           <nav
@@ -88,12 +112,13 @@ export default function CustomTable() {
             </span>
             <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
               <li>
-                <a
-                  href="#"
+                <button
+                  onClick={() => setCurrentPage(currentPage - 1)}
+                  disabled={currentPage}
                   className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   Previous
-                </a>
+                </button>
               </li>
               <li>
                 <a
@@ -137,12 +162,13 @@ export default function CustomTable() {
                 </a>
               </li>
               <li>
-                <a
-                  href="#"
+                <button
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                  disabled={currentPage == numberOfPages}
                   className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   Next
-                </a>
+                </button>
               </li>
             </ul>
           </nav>
--- src/app/components/back-office/Navbar.jsx
+++ src/app/components/back-office/Navbar.jsx
@@ -1,5 +1,13 @@
 import { AlignJustify, Bell, Sun, User } from "lucide-react";
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Navbar() {
   return (
@@ -11,12 +19,43 @@ export default function Navbar() {
         <button>
           <Sun />
         </button>
-        <button>
-          <Bell />
-        </button>
-        <button>
-          <User />
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+          <button
+            type="button"
+            className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <Bell />
+            <span className="sr-only">Notifications</span>
+            <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 ">
+              2
+            </div>
+          </button>   
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Notifications</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Dash Board make component </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+       
+       
+       
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            {" "}
+            <button>
+              <User />
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Profile</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Dash Board</DropdownMenuItem>
+            <DropdownMenuItem>Edit Profile</DropdownMenuItem>
+            <DropdownMenuItem>Log Out</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
